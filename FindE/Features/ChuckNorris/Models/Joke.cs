324365256d2f12afb5048686f68476a8ed6d118b
Elton Monteiro <elton@sistemasbr.net>
2022-06-13 23:30:04 -0300

    Add project files.

---
+++ FindE/Features/ChuckNorris/Models/Joke.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace FindE.Features.ChuckNorris.Models
+{
+    public class Joke
+    {
+        [JsonProperty("value")]
+        public string Piada { get; set; }
+        public string Erro { get; set; }
+    }
+}
---
+++ FindE/Program.cs
@@ -0,0 +1,35 @@
+using Blazored.Toast;
+using FindE.Features.Cep.Services;
+using FindE.Features.ChuckNorris.Services;
+using FindE.Features.PrevisaoDoTempo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredToast();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<CepService>();
+builder.Services.AddSingleton<ChuckNorrisService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
