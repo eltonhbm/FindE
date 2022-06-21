27e1b1e4166050f6361bfb79825223bee70c970f
Gui-Angelo-Silva <100084412+Gui-Angelo-Silva@users.noreply.github.com>
2022-06-21 08:28:16 -0300

    Erro corrigdo do cadastro

--- FindE/Features/CadastroEmpresa/EmpresaModel/CadastroEmpresa.cs
+++ FindE/Features/CadastroEmpresa/EmpresaModel/CadastroEmpresa.cs
@@ -2,10 +2,11 @@
 {
     public class CadastroEmpresa
     {
-        public int Cadastro { get; set; }   
+        public int IdCadastro { get; set; }   
         public string Nome { get; set; }
         public string Responsavel { get; set; }
         public string Email { get; set; }
-        public string Whattsap { get; set; }
+        public string Whattsap { get; set; } 
+        public string FotoEmpresa  { get; set; }
     }
 }
