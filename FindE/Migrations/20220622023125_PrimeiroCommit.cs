61a8be283e08e4c239207bfc2485adb7e05b535a
Elton Monteiro <elton@sistemasbr.net>
2022-06-22 00:33:24 -0300

    Listando e gravando contas

---
+++ FindE/Migrations/20220622023125_PrimeiroCommit.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindE.Migrations
+{
+    public partial class PrimeiroCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Usuario = table.Column<string>(type: "TEXT", nullable: false),
+                    Senha = table.Column<string>(type: "TEXT", nullable: false),
+                    Perfil = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conta", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Conta",
+                columns: new[] { "Id", "Perfil", "Senha", "Usuario" },
+                values: new object[] { 1, 0, "af023865c44e4fcf1fd229c1788f74f07e7fc518ea2b8c33c0f9d903bea6b3ff", "sistema" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conta");
+        }
+    }
+}
