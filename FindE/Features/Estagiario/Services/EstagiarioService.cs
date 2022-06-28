348394f031297f449c7a61c169da6d8b26f8bfdb
EduardoLoppes <mcfgonzalles@gmail.com>
2022-06-27 23:51:13 -0300

    CRUD de estagiario
    criei os cruds e estao quase funcionais só o banco de dados que não consegui fazer funcionar

--- FindE/Features/Estagiario/Services/EstagiarioService.cs
+++ FindE/Features/Estagiario/Services/EstagiarioService.cs
@@ -1,6 +1,86 @@
-﻿namespace FindE.Features.Estagiario.Services
+﻿using FindE.Data;
+using FindE.Features.Estagiario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindE.Features.Estagiario.Services;
+
+public class EstagiarioService
 {
-    public class EstagiarioService
+    private AppDbContext dbContext;
+
+    public EstagiarioService(AppDbContext dbContext)
     {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<List<EstagiarioModel>> ListarEstagiarios()
+    {
+        try
+        {
+            return await dbContext.Estagiario.ToListAsync();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<EstagiarioModel> InserirEstagiario(EstagiarioModel estagiario)
+    {
+        try
+        {
+            dbContext.Estagiario.Add(estagiario);
+            await dbContext.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+        return estagiario;
+    }
+
+    public async Task<EstagiarioModel> AlterarEstagiario(EstagiarioModel estagiario)
+    {
+        try
+        {
+            if (dbContext.Estagiario.FirstOrDefault(c => c.Id == estagiario.Id) != null)
+            {
+                dbContext.Estagiario.Update(estagiario);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+        return estagiario;
+    }
+
+    public async Task ApagarEstagiario(EstagiarioModel estagiario)
+    {
+        try
+        {
+            dbContext.Estagiario.Remove(estagiario);
+            await dbContext.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<EstagiarioModel> RetornarEstagiario(int IdEstagiario)
+    {
+        try
+        {
+            var estagiarios = await dbContext.Estagiario.Where(c => c.Id == IdEstagiario).ToListAsync();
+            return estagiarios.Single();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 }
